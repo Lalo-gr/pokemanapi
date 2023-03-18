e472f023bc948d23a52ef4263b9383d89e3f8be4
Lalo-gr <einajera741@gmail.com>
2023-03-18 18:12:16 -0500

    "Final working Pokedex"

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,19 @@ function App() {
       <button onClick={searchPokemon}>Search Pokeman</button>
     </div>
     <div className="DisplaySection">
-      {!pokemonChosen ? (<h1> Please choose a pokemon</h1>) : (<h1>{pokemonName}</h1>)}
+      {!pokemonChosen ? (
+      <h1> Please choose a pokemon</h1>
+      ) : (
+      <>
+      <h1>{pokemon.name}</h1>
+      <img src={pokemon.img} alt="Image of pokemon" />
+      <h3>Species: {pokemon.species}</h3>
+      <h3>Type: {pokemon.type}</h3>
+      <h4>Hp: {pokemon.hp}</h4>
+      <h4>Attack: {pokemon.attack}</h4>
+      <h4>Defense: {pokemon.defense}</h4>
+      </>
+      )}
     </div>
     </div>
     );
